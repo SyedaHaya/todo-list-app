81e996cbc25ec68563e2ea1f82710aa63f0e67d3
SyedaHaya <141368715+SyedaHaya@users.noreply.github.com>
2023-09-23 02:44:32 +0500

    todo-list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [task, setTask] = useState('');
+
+  const addTask = () => {
+    if (task.trim() !== '') {
+      setTodoList([...todoList, task]);
+      setTask('');
+    }
+  };
+
+  const editTask = (ind) => {
+    const update = prompt('Enter a new value');
+    if (update !== null) {
+      const updatedList = [...todoList];
+      updatedList[ind] = update;
+      setTodoList(updatedList);
+    }
+  };
+
+  const removeTask = (ind) => {
+    const updatedList = [...todoList];
+    updatedList.splice(ind, 1);
+    setTodoList(updatedList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo App</h1>
+      <div className='card-1'>
+        <input className='card'
+          type="text"
+          placeholder="Add a task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button className='card2' onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {todoList.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button className='card3' onClick={() => editTask(index)}>Edit</button>
+            <button className='card4' onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
